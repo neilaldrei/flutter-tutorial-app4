713a70920bb480c4ef9b1a2ddcceba83aa2c3861
neilaldrei <neilaldreiiv@gmail.com>
2020-03-15 07:57:25 +0800

    Lesson #24: Widget Lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,25 @@ void main() => runApp(MaterialApp(
     '/location': (context) => ChooseLocation(),
   }
 ));
+
+/*
+  Widget Lifecycle
+
+
+  Stateless 
+  - State does not change overtime
+  - Build function only runs once
+
+  Stateful
+  - State CAN change over time
+  - SetState() triggers the build function
+
+  _ initState() 
+    > Called only once when widget is created
+    > Subscribe to streams or any object that could change our widget data
+  _ Build()
+    > Builds the widget tree
+    > A build is triggered every time we use setState()
+  _ Dispose()
+    > When the widget / state is removed
+ */
