bed7466ccaa8415cbb69fc5c8e5e60995f59a762
neilaldrei <neilaldreiiv@gmail.com>
2020-03-15 08:31:57 +0800

    Lesson #26: Flutter Packages (http)

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,28 +7,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void getData() async {
-    
-    // Simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'Yoshi';
-    });
-
-    // Simulate network request for a user bio
-    String bio = await Future.delayed(Duration(seconds: 1), () {
-      return 'Yoshi likes banana chips';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
