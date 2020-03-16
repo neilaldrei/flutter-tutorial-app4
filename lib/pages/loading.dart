4be46bf4d7948562311bb1ba4cf3f5ee2843c80f
neilaldrei <neilaldreiiv@gmail.com>
2020-03-16 08:44:19 +0800

    Lesson #27: World time API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,9 +12,23 @@ class _LoadingState extends State<Loading> {
 
   void getData() async {
     
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    // make request from worldtimeapi
+    Response response = await get('http://worldtimeapi.org/api/timezone/Asia/Manila');
     Map data = jsonDecode(response.body);
-    print(data['title']);
+    // print(data);
+
+    // get props from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // print(datetime);
+    // print(offset);
+
+    // Create date time object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+    
   }
 
   @override
