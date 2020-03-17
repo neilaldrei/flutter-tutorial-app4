3f083129811b8a676ff8cc80361d4df352b8a85e
neilaldrei <neilaldreiiv@gmail.com>
2020-03-17 18:49:51 +0800

    Lesson #33: Ternary operators

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -6,15 +6,15 @@ class WorldTime {
 
   String location;
   String time;
-  String flag; // URL to an assets folder
-  String url; // Location for world time api end points
+  String flag;    // URL to an assets folder
+  String url;     // Location for world time api end points
+  bool isDaytime; // Check if it's daytime
 
-  WorldTime({ this.location, this.flag, this.url });
+  WorldTime({ this.location, this.flag, this.url, this.isDaytime });
 
   Future<void> getTime() async {
 
     try {
-
       // make request from worldtimeapi
       Response response = await get('http://worldtimeapi.org/api/timezone/$url');
       Map data = jsonDecode(response.body);
@@ -27,6 +27,9 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
       
+      // Ternary Operator
+      isDaytime = now.hour > 6 && now.hour < 18 ? true : false;
+
       // Store date time object to time var
       time = DateFormat.jm().format(now);
 
@@ -34,6 +37,5 @@ class WorldTime {
       print('Caught Error: $e');
       time = "Could not get time data";
     }
-    
   }
 }
